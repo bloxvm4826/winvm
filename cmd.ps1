d7c5a83e839cd1a7f21d186c4b393af049c4f41e
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 11:51:40 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,8 +1,8 @@
 . C:\l.ps1
 . C:\bot.ps1
 Act
-Respawn
-Start-Sleep 6
+$null = Click 681 476 250
+Start-Sleep 8
 Add-Type -AssemblyName System.Drawing
 $b=New-Object Drawing.Bitmap 1600,900
 $g=[Drawing.Graphics]::FromImage($b); $g.CopyFromScreen(0,0,0,0,(New-Object Drawing.Size 1600,900))
