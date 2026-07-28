35373af1171e4c61bbb7e9a873f6211fe9038253
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 04:40:18 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,3 +1,12 @@
-CClick 799 549
-Start-Sleep 3
-"ok clicked"
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class W2 {
+  [DllImport("user32.dll")] public static extern bool SetCursorPos(int x, int y);
+  [DllImport("user32.dll")] public static extern void mouse_event(uint f, uint dx, uint dy, int d, int e);
+}
+"@ -EA SilentlyContinue
+function Scroll([int]$x,[int]$y,[int]$notches){ Act; [W2]::SetCursorPos($x,$y); Start-Sleep -m 200; for($i=0;$i -lt [Math]::Abs($notches);$i++){ [W2]::mouse_event(0x0800,0,0,(120*[Math]::Sign($notches)),0); Start-Sleep -m 120 } }
+Scroll 800 450 -5
+Start-Sleep 2
+"scrolled"
