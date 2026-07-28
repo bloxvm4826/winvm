0fdb75dae08800ad5e1683b8dc12096ed2a1d0fd
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 12:03:16 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,4 +1,5 @@
-try { $r = irm "https://raw.githubusercontent.com/bloxvm4826/winvm/main/auto.ps1?v=11"; "len=" + $r.Length } catch { "ERR: " + $_.Exception.Message }
+foreach($f in "bot.ps1","auto.ps1"){ irm "https://github.com/bloxvm4826/winvm/raw/main/$f" -OutFile "C:\$f" }
+"{0} {1}" -f (Get-Item C:\bot.ps1).Length, (Get-Item C:\auto.ps1).Length
 Add-Type -AssemblyName System.Drawing
 $b=New-Object Drawing.Bitmap 1600,900
 $g=[Drawing.Graphics]::FromImage($b); $g.CopyFromScreen(0,0,0,0,(New-Object Drawing.Size 1600,900))
