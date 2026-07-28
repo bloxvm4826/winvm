489c4ff04b46f9c51207297e62bc8e9862e09a6c
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 12:01:44 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,6 +1,4 @@
-irm "https://raw.githubusercontent.com/bloxvm4826/winvm/main/bot.ps1" -OutFile C:\bot.ps1
-irm "https://raw.githubusercontent.com/bloxvm4826/winvm/main/auto.ps1" -OutFile C:\auto.ps1
-"{0} {1}" -f (Get-Item C:\bot.ps1).Length, (Get-Item C:\auto.ps1).Length
+"alive " + (Get-Date -Format HH:mm:ss)
 Add-Type -AssemblyName System.Drawing
 $b=New-Object Drawing.Bitmap 1600,900
 $g=[Drawing.Graphics]::FromImage($b); $g.CopyFromScreen(0,0,0,0,(New-Object Drawing.Size 1600,900))
