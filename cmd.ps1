a28c3c043d69d63eca39bb2aeda52e503bacbe88
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 13:11:34 -0400

    u

--- cmd.ps1
+++ cmd.ps1
@@ -1,8 +1,16 @@
-'TAG:T1785258425'
-$t=[int](Get-Date -UFormat %s)
-irm "https://raw.githubusercontent.com/bloxvm4826/winvm/main/play2.ps1?t=$t" -OutFile C:\play2.ps1
-Remove-Item C:\play2.log -EA 0
-Start-Process powershell -ArgumentList "-ExecutionPolicy","Bypass","-WindowStyle","Hidden","-File","C:\play2.ps1","-Minutes","70" -WindowStyle Hidden
-Start-Sleep -m 4000
-"launched; log:"
-if(Test-Path C:\play2.log){Get-Content C:\play2.log}else{"(no log yet)"}
+'TAG:T1785258694'
+(Get-Content C:\play2.log -Tail 5) -join " / "
+Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+$b=[System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+$bmp=New-Object Drawing.Bitmap $b.Width,$b.Height
+$g=[Drawing.Graphics]::FromImage($bmp)
+$g.CopyFromScreen($b.Location,[Drawing.Point]::Empty,$b.Size)
+$sm=New-Object Drawing.Bitmap 1280,720
+$g2=[Drawing.Graphics]::FromImage($sm)
+$g2.DrawImage($bmp,0,0,1280,720)
+$ms=New-Object IO.MemoryStream
+$enc=[Drawing.Imaging.ImageCodecInfo]::GetImageEncoders()|Where-Object {$_.MimeType -eq 'image/jpeg'}
+$p=New-Object Drawing.Imaging.EncoderParameters 1
+$p.Param[0]=New-Object Drawing.Imaging.EncoderParameter ([Drawing.Imaging.Encoder]::Quality),50
+$sm.Save($ms,$enc,$p)
+"B64:"+[Convert]::ToBase64String($ms.ToArray())
