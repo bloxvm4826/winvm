5aaa6d858d83356a4ba68c97c3225ba37cb591b6
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 11:37:50 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,28 +1,14 @@
 . C:\l.ps1
 . C:\bot.ps1
-Add-Type -AssemblyName System.Windows.Forms
 Act
-$null = Click 1405 126
-Start-Sleep 4
-$null = Click 796 446
-Start-Sleep 2
-foreach($i in 1..2){ $null=KeyDown 0x28; Start-Sleep -m 80; $null=KeyUp 0x28; Start-Sleep -m 250 }
-$null=KeyDown 0x0D; Start-Sleep -m 80; $null=KeyUp 0x0D
-Start-Sleep 2
-$null = Click 852 588 150
+$null = Click 432 464 150
+Start-Sleep 6
+$null = Click 885 521 150
+Start-Sleep 30
+$null = Click 799 549 150
 Start-Sleep 3
-$null = Click 796 450
-Start-Sleep 1
-[System.Windows.Forms.SendKeys]::SendWait("somanyalts30@gmail.com")
-Start-Sleep 1
-$null = Click 796 519
-Start-Sleep 1
-[System.Windows.Forms.SendKeys]::SendWait("Azerty060809*")
-Start-Sleep 1
-$null = Click 796 588 150
-Start-Sleep 8
-$null = Click 1446 371
-Start-Sleep 2
+$null = Click 875 116 150
+Start-Sleep 5
 Add-Type -AssemblyName System.Drawing
 $b=New-Object Drawing.Bitmap 1600,900
 $g=[Drawing.Graphics]::FromImage($b); $g.CopyFromScreen(0,0,0,0,(New-Object Drawing.Size 1600,900))
