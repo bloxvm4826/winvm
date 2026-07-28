195bc53cc531906a0a622fad3ad463a4c9c2b721
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 13:01:19 -0400

    u

--- cmd.ps1
+++ cmd.ps1
@@ -1,16 +1,6 @@
-'TAG:T1785258041'
-"procs: "+((Get-Process chrome -EA 0).Count)+" auto="+((Get-Process powershell,pwsh -EA 0|Measure).Count)
-Add-Type -AssemblyName System.Windows.Forms,System.Drawing
-$b=[System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-$bmp=New-Object Drawing.Bitmap $b.Width,$b.Height
-$g=[Drawing.Graphics]::FromImage($bmp)
-$g.CopyFromScreen($b.Location,[Drawing.Point]::Empty,$b.Size)
-$sm=New-Object Drawing.Bitmap 1280,720
-$g2=[Drawing.Graphics]::FromImage($sm)
-$g2.DrawImage($bmp,0,0,1280,720)
-$ms=New-Object IO.MemoryStream
-$enc=[Drawing.Imaging.ImageCodecInfo]::GetImageEncoders()|Where-Object {$_.MimeType -eq 'image/jpeg'}
-$p=New-Object Drawing.Imaging.EncoderParameters 1
-$p.Param[0]=New-Object Drawing.Imaging.EncoderParameter ([Drawing.Imaging.Encoder]::Quality),50
-$sm.Save($ms,$enc,$p)
-"B64:"+[Convert]::ToBase64String($ms.ToArray())
+'TAG:T1785258078'
+(Get-ChildItem C:\ -Filter *.ps1 -EA 0|%{$_.Name+" "+$_.Length}) -join " | "
+"---log---"
+if(Test-Path C:\auto.log){(Get-Content C:\auto.log -Tail 4) -join " / "}else{"no auto.log"}
+"---running---"
+(Get-CimInstance Win32_Process -Filter "Name=$([char]39)pwsh.exe$([char]39) or Name=$([char]39)powershell.exe$([char]39)"|%{$_.ProcessId.ToString()+":"+($_.CommandLine -replace "\s+"," ").Substring(0,[Math]::Min(70,$_.CommandLine.Length))}) -join "`n"
