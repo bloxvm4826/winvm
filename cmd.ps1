fcaaa0830a83f741687cff0bd2841a2feca4996d
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 04:36:56 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,14 +1,3 @@
-Add-Type -AssemblyName System.Windows.Forms
-Set-Clipboard -Value 'somanyalts30@gmail.com'
-CClick 796 449
-Start-Sleep 1
-[System.Windows.Forms.SendKeys]::SendWait("^a")
-[System.Windows.Forms.SendKeys]::SendWait("^v")
-Start-Sleep 1
-Set-Clipboard -Value 'Azerty060809*'
-CClick 796 519
-Start-Sleep 1
-[System.Windows.Forms.SendKeys]::SendWait("^a")
-[System.Windows.Forms.SendKeys]::SendWait("^v")
-Start-Sleep 1
-"typed creds"
+CClick 796 586
+Start-Sleep 6
+"signed in"
