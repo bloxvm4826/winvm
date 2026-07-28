0efd5c461039be82796316864e8f6fc0be7de757
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 10:43:39 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,8 +1,10 @@
 . C:\l.ps1
 . C:\bot.ps1
 Act
-$null=KeyDown 0x11; Start-Sleep -m 60; $null=KeyDown 0x4D; Start-Sleep -m 80; $null=KeyUp 0x4D; Start-Sleep -m 60; $null=KeyUp 0x11
-Start-Sleep 3
+$null=KeyDown 0x57; Start-Sleep 2; $null=KeyUp 0x57
+$null=KeyDown 0x20; Start-Sleep -m 200; $null=KeyUp 0x20
+$null=KeyDown 0x53; Start-Sleep 2; $null=KeyUp 0x53
+Start-Sleep 1
 Add-Type -AssemblyName System.Drawing
 $b=New-Object Drawing.Bitmap 1600,900
 $g=[Drawing.Graphics]::FromImage($b); $g.CopyFromScreen(0,0,0,0,(New-Object Drawing.Size 1600,900))
