8716a4d52411d4c8a18c733311224e364d97222f
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 04:36:29 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,3 +1,14 @@
-CClick 853 588
-Start-Sleep 3
-"clicked sign in with password"
+Add-Type -AssemblyName System.Windows.Forms
+Set-Clipboard -Value 'somanyalts30@gmail.com'
+CClick 796 449
+Start-Sleep 1
+[System.Windows.Forms.SendKeys]::SendWait("^a")
+[System.Windows.Forms.SendKeys]::SendWait("^v")
+Start-Sleep 1
+Set-Clipboard -Value 'Azerty060809*'
+CClick 796 519
+Start-Sleep 1
+[System.Windows.Forms.SendKeys]::SendWait("^a")
+[System.Windows.Forms.SendKeys]::SendWait("^v")
+Start-Sleep 1
+"typed creds"
