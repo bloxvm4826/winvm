7687ebb5ac4ce7f48761d9c6adc09c6b23b82ff0
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 13:28:53 -0400

    u

--- cmd.ps1
+++ cmd.ps1
@@ -1,22 +1,8 @@
-'TAG:T1785259672'
-. C:\l.ps1
-Act
-Click 346 124 250; Start-Sleep -m 900     # close the stray compact menu
-Click 346 124 250; Start-Sleep -m 900     # reopen it deliberately
-Click 365 430 250; Start-Sleep -m 1000    # Respawn
-Click 681 476 250; Start-Sleep -m 6000    # confirm + let the teleport settle
-"respawn attempted"
-Add-Type -AssemblyName System.Windows.Forms,System.Drawing
-$b=[System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-$bmp=New-Object Drawing.Bitmap $b.Width,$b.Height
-$g=[Drawing.Graphics]::FromImage($bmp)
-$g.CopyFromScreen($b.Location,[Drawing.Point]::Empty,$b.Size)
-$sm=New-Object Drawing.Bitmap 1280,720
-$g2=[Drawing.Graphics]::FromImage($sm)
-$g2.DrawImage($bmp,0,0,1280,720)
-$ms=New-Object IO.MemoryStream
-$enc=[Drawing.Imaging.ImageCodecInfo]::GetImageEncoders()|Where-Object {$_.MimeType -eq 'image/jpeg'}
-$p=New-Object Drawing.Imaging.EncoderParameters 1
-$p.Param[0]=New-Object Drawing.Imaging.EncoderParameter ([Drawing.Imaging.Encoder]::Quality),50
-$sm.Save($ms,$enc,$p)
-"B64:"+[Convert]::ToBase64String($ms.ToArray())
+'TAG:T1785259733'
+Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -like "*play3.ps1*" -or $_.CommandLine -like "*play2.ps1*" } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force -EA 0 }
+$t=[int](Get-Date -UFormat %s)
+irm "https://raw.githubusercontent.com/bloxvm4826/winvm/main/play4.ps1?t=$t" -OutFile C:\play4.ps1
+Remove-Item C:\play4.log -EA 0
+Start-Process powershell -ArgumentList "-ExecutionPolicy","Bypass","-WindowStyle","Hidden","-File","C:\play4.ps1","-Minutes","215" -WindowStyle Hidden
+Start-Sleep -m 3000
+"keepalive: " + ((Get-Content C:\play4.log -EA 0) -join " / ")
