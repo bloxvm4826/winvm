2fdd5b11094191bd4728dc0252dfac082da15372
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 13:01:49 -0400

    u

--- cmd.ps1
+++ cmd.ps1
@@ -1,6 +1,2 @@
-'TAG:T1785258078'
-(Get-ChildItem C:\ -Filter *.ps1 -EA 0|%{$_.Name+" "+$_.Length}) -join " | "
-"---log---"
-if(Test-Path C:\auto.log){(Get-Content C:\auto.log -Tail 4) -join " / "}else{"no auto.log"}
-"---running---"
-(Get-CimInstance Win32_Process -Filter "Name=$([char]39)pwsh.exe$([char]39) or Name=$([char]39)powershell.exe$([char]39)"|%{$_.ProcessId.ToString()+":"+($_.CommandLine -replace "\s+"," ").Substring(0,[Math]::Min(70,$_.CommandLine.Length))}) -join "`n"
+'TAG:T1785258109'
+Get-Content C:\l.ps1 -Raw
