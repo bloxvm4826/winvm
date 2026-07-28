179f75741d41af8d1d86284e253d6505d75a465f
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 10:10:11 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,12 +1,5 @@
-. C:\l.ps1
-. C:\bot.ps1
-Act
-$null = Click 894 524 250
-Start-Sleep 2
-$null = Click 304 324 250
-Start-Sleep 2
-Grind 7
-Write-Host ("done " + (Get-Date -Format HH:mm:ss))
+Write-Host ("up " + (Get-Date -Format HH:mm:ss) + " " + $env:COMPUTERNAME)
+Write-Host (Test-Path C:\l.ps1)
 Add-Type -AssemblyName System.Drawing
 $b=New-Object Drawing.Bitmap 1600,900
 $g=[Drawing.Graphics]::FromImage($b); $g.CopyFromScreen(0,0,0,0,(New-Object Drawing.Size 1600,900))
