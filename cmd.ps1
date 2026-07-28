dd5e780517e1602614d19d8a016d189d54052c70
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 04:33:28 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,6 +1,3 @@
-iwr https://raw.githubusercontent.com/bloxvm4826/winvm/main/lib.ps1 -o C:\l.ps1
-. C:\l.ps1
-Add-Type -AssemblyName System.Windows.Forms
-$s=[System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-"screen: $($s.Width)x$($s.Height)"
-$h=Hwnd; "chrome hwnd: $h  zoomed:" + [W]::IsZoomed($h)
+CClick 1405 126
+Start-Sleep 2
+"clicked sign in"
