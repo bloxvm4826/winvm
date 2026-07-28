7859ac5deeb2ff2bf020836ff3ac7c8df807b1e0
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 05:34:02 -0400

    x

--- cmd.ps1
+++ cmd.ps1
@@ -1,7 +1,7 @@
-CClick 799 529; Start-Sleep 2
+CClick 929 574; Start-Sleep 3
 Click 799 529; Start-Sleep 2
-Click 1187 117; Start-Sleep 3
-"skip+reopen"
+Click 1026 244; Start-Sleep 2
+"largecrate+close"
 Add-Type -AssemblyName System.Drawing
 $b=New-Object Drawing.Bitmap 1600,900
 $g=[Drawing.Graphics]::FromImage($b); $g.CopyFromScreen(0,0,0,0,(New-Object Drawing.Size 1600,900))
