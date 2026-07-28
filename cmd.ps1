9c65ba8eac6dcdef18cee14cd0009a2572de6a11
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 13:18:49 -0400

    u

--- cmd.ps1
+++ cmd.ps1
@@ -1,14 +1,5 @@
-'TAG:T1785258813'
-. C:\l.ps1
-Act
-Click 346 124 250        # close the compact menu left open
-Start-Sleep -m 800
-$t=[int](Get-Date -UFormat %s)
-irm "https://raw.githubusercontent.com/bloxvm4826/winvm/main/play2.ps1?t=$t" -OutFile C:\play2.ps1
-Remove-Item C:\play2.log -EA 0
-Start-Process powershell -ArgumentList "-ExecutionPolicy","Bypass","-WindowStyle","Hidden","-File","C:\play2.ps1","-Minutes","60" -WindowStyle Hidden
-Start-Sleep -m 3000
-"relaunched"
+'TAG:T1785259129'
+"log: "+((Get-Content C:\play2.log -Tail 4) -join " / ")
 Add-Type -AssemblyName System.Windows.Forms,System.Drawing
 $b=[System.Windows.Forms.Screen]::PrimaryScreen.Bounds
 $bmp=New-Object Drawing.Bitmap $b.Width,$b.Height
