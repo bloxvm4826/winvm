46792cc5f72bedcd458e8d43684e63dff16d9704
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 13:27:13 -0400

    u

--- cmd.ps1
+++ cmd.ps1
@@ -1,5 +1,15 @@
-'TAG:T1785259604'
-"log: "+((Get-Content C:\play3.log -Tail 3) -join " / ")
+'TAG:T1785259632'
+Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -like "*play3.ps1*" } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force -EA 0 }
+Start-Sleep -m 600
+. C:\l.ps1
+function Key([byte]$vk,[int]$hold=60){ [K]::keybd_event($vk,0,0,0); Start-Sleep -m $hold; [K]::keybd_event($vk,0,2,0); Start-Sleep -m 250 }
+Act
+# full respawn to get out of the water, then close every overlay
+Click 346 124 250; Start-Sleep -m 700
+Click 365 430 250; Start-Sleep -m 900
+Click 681 476 250; Start-Sleep -m 4500
+Click 346 124 250; Start-Sleep -m 900
+"sorties done: " + ((Get-Content C:\play3.log | Select-String "sortie").Count)
 Add-Type -AssemblyName System.Windows.Forms,System.Drawing
 $b=[System.Windows.Forms.Screen]::PrimaryScreen.Bounds
 $bmp=New-Object Drawing.Bitmap $b.Width,$b.Height
