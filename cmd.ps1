ff1f77eadad85a1a0990bfd7e8fd32c2eed39c3a
bloxvm4826 <filthysignal546@mail.bu.app>
2026-07-28 13:00:29 -0400

    u

--- cmd.ps1
+++ cmd.ps1
@@ -1,15 +1,2 @@
-Remove-Item C:\auto.log -ErrorAction SilentlyContinue
-Start-Process powershell -ArgumentList "-ExecutionPolicy","Bypass","-File","C:\auto.ps1" -WindowStyle Hidden
-Start-Sleep 20
-(Get-Content C:\auto.log -ErrorAction SilentlyContinue) -join " | "
-Add-Type -AssemblyName System.Drawing
-$b=New-Object Drawing.Bitmap 1600,900
-$g=[Drawing.Graphics]::FromImage($b); $g.CopyFromScreen(0,0,0,0,(New-Object Drawing.Size 1600,900))
-$s=New-Object Drawing.Bitmap 1280,720
-$g2=[Drawing.Graphics]::FromImage($s); $g2.DrawImage($b,0,0,1280,720)
-$ms=New-Object IO.MemoryStream
-$enc=[Drawing.Imaging.ImageCodecInfo]::GetImageEncoders()|?{$_.MimeType -eq 'image/jpeg'}
-$p=New-Object Drawing.Imaging.EncoderParameters 1
-$p.Param[0]=New-Object Drawing.Imaging.EncoderParameter ([Drawing.Imaging.Encoder]::Quality),40
-$s.Save($ms,$enc,$p)
-"B64:"+[Convert]::ToBase64String($ms.ToArray())
+'TAG:T1785258029'
+"alive: "+(Get-Date -F HH:mm:ss)+" user="+$env:USERNAME
